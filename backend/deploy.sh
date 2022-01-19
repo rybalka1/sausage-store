65e7936af4a0be1e587b9383cbd2b8d048c25c88
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-01-19 15:52:39 +0300

    Add service "Sausage-store"

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,12 +2,11 @@
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
-# sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-# sudo rm -f /home/jarservice/sausage-store.jar||true
-#Переносим артефакт в нужную папку
+sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo mkdir /opt/log |true
 sudo cp ./sausage-store-0.0.1-SNAPSHOT.jar $(pwd)/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
 # sudo chown -R jarservice.jarservice /home/jarservice/sausage-store.jar
 #Обновляем конфиг systemd с помощью рестарта
-# sudo systemctl daemon-reload
+sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
-# sudo systemctl restart sausage-store-backend
+sudo systemctl restart sausage-store-backend
