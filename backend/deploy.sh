d289fba76de70a2fa79b4ff4f6d0a432280f0000
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-02-15 17:02:03 +0300

    Test-003

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,8 +7,9 @@ sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-ba
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.jar
 sudo cp ./sausage-store-$VERSION.jar /home/jarservice/sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
 sudo mkdir /etc/systemd/system/sausage-store-backend.service.d || true
-
-sudo cat <<EOF >/etc/systemd/system/sausage-store-backend.service.d/override.conf
+sudo touch /etc/systemd/system/sausage-store-backend.service.d/override.conf
+sudo -s
+cat <<EOF >/etc/systemd/system/sausage-store-backend.service.d/override.conf
 Environment="PSQL_HOST=${PSQL_HOST}"
 Environment="PSQL_PORT=6432"
 Environment="PSQL_DBNAME=${PSQL_DBNAME}"
@@ -19,7 +20,7 @@ Environment="MONGO_PASSWORD=${MONGO_PASSWORD}"
 Environment="MONGO_URL=${MONGO_URL}"
 Environment="MONGO_DATABASE=${MONGO_DATABASE}"
 EOF
-
+exit
 sudo chown -R student:student /home/student/sausage-store.jar
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
