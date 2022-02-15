a8559de26c675be6b8ddf2baa41b1369d10c8b29
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-02-15 17:14:23 +0300

    Test-004

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,6 +2,16 @@
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
 VERSION=${VERSION}
+PSQL_HOST=${PSQL_HOST}
+PSQL_DBNAME=${PSQL_DBNAME}
+PSQL_USER=${PSQL_USER}
+PSQL_PASSWORD=${PSQL_PASSWORD}
+MONGO_USER=${MONGO_USER}
+MONGO_PASSWORD=${MONGO_PASSWORD}
+MONGO_URL=${MONGO_URL}
+MONGO_DATABASE=${MONGO_DATABASE}
+
+
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.jar
@@ -10,15 +20,15 @@ sudo mkdir /etc/systemd/system/sausage-store-backend.service.d || true
 sudo touch /etc/systemd/system/sausage-store-backend.service.d/override.conf
 sudo -s
 cat <<EOF >/etc/systemd/system/sausage-store-backend.service.d/override.conf
-Environment="PSQL_HOST=${PSQL_HOST}"
+Environment="PSQL_HOST=$PSQL_HOST"
 Environment="PSQL_PORT=6432"
-Environment="PSQL_DBNAME=${PSQL_DBNAME}"
-Environment="PSQL_USER=${PSQL_USER}"
-Environment="PSQL_PASSWORD=${PSQL_PASSWORD}"
-Environment="MONGO_USER=${MONGO_USER}"
-Environment="MONGO_PASSWORD=${MONGO_PASSWORD}"
-Environment="MONGO_URL=${MONGO_URL}"
-Environment="MONGO_DATABASE=${MONGO_DATABASE}"
+Environment="PSQL_DBNAME=$PSQL_DBNAME"
+Environment="PSQL_USER=$PSQL_USER"
+Environment="PSQL_PASSWORD=$PSQL_PASSWORD"
+Environment="MONGO_USER=$MONGO_USER}"
+Environment="MONGO_PASSWORD=$MONGO_PASSWORD"
+Environment="MONGO_URL=$MONGO_URL"
+Environment="MONGO_DATABASE=$MONGO_DATABASE"
 EOF
 exit
 sudo chown -R student:student /home/student/sausage-store.jar
