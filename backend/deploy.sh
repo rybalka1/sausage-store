81d85cf5243a161943b2da41feceed7f28b49153
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-02-15 21:05:04 +0300

    Test-013

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -16,18 +16,20 @@ sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-ba
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.jar
 sudo cp ./sausage-store-$VERSION.jar /home/jarservice/sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
 sudo mkdir /etc/systemd/system/sausage-store-backend.service.d || true
-sudo touch /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo "" > /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="PSQL_HOST=${PSQL_HOST}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="PSQL_PORT=6432"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="PSQL_DBNAME=${PSQL_DBNAME}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="PSQL_USER=${PSQL_USER}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="PSQL_PASSWORD=${PSQL_PASSWORD}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="MONGO_USER=${MONGO_USER}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="MONGO_PASSWORD=${MONGO_PASSWORD}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="MONGO_URL=${MONGO_URL}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo echo 'Environment="MONGO_DATABASE=${MONGO_DATABASE}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo chown -R student:student /home/student/sausage-store.jar
+sudo -s
+touch /etc/systemd/system/sausage-store-backend.service.d/override.conf || true
+echo "" > /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="PSQL_HOST=${PSQL_HOST}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="PSQL_PORT=6432"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="PSQL_DBNAME=${PSQL_DBNAME}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="PSQL_USER=${PSQL_USER}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="PSQL_PASSWORD=${PSQL_PASSWORD}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="MONGO_USER=${MONGO_USER}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="MONGO_PASSWORD=${MONGO_PASSWORD}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="MONGO_URL=${MONGO_URL}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+echo 'Environment="MONGO_DATABASE=${MONGO_DATABASE}"' >> /etc/systemd/system/sausage-store-backend.service.d/override.conf
+exit
+chown -R student:student /home/student/sausage-store.jar
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
