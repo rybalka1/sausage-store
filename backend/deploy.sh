240ca1ff4a1ea7bc9145626eab84d4c9814764d7
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-02-15 16:36:22 +0300

    Test-001

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,12 +4,23 @@ set -xe
 VERSION=${VERSION}
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-# sudo cp -rf ./sausage-store-logrotate /etc/logrotate.d/sausage-store-logrotate
-sudo mkdir /opt/log || true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.jar
-
 sudo cp ./sausage-store-$VERSION.jar /home/jarservice/sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
-sudo chown -R jarservice.jarservice /home/jarservice/sausage-store.jar
+sudo mkdir /etc/systemd/system/sausage-store-backend.service.d || true
+
+sudo cat <<EOF>  /etc/systemd/system/sausage-store-backend.service.d/override.conf
+Environment="PSQL_HOST=${PSQL_HOST}"
+Environment="PSQL_PORT=6432"
+Environment="PSQL_DBNAME=${PSQL_DBNAME}"
+Environment="PSQL_USER=${PSQL_USER}"
+Environment="PSQL_PASSWORD=${PSQL_PASSWORD}"
+Environment="MONGO_USER=${MONGO_USER}"
+Environment="MONGO_PASSWORD=${MONGO_PASSWORD}"
+Environment="MONGO_URL=${MONGO_URL}"
+Environment="MONGO_DATABASE=${MONGO_DATABASE}"
+EOF
+
+sudo chown -R student:student /home/student/sausage-store.jar
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
