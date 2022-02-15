9cba98766ca8afc990bd0ff77e6766f1f656d632
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-02-15 17:52:07 +0300

    Test-006

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,14 +2,6 @@
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
 VERSION=${VERSION}
-
-#Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.jar
-sudo cp ./sausage-store-$VERSION.jar /home/jarservice/sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
-sudo mkdir /etc/systemd/system/sausage-store-backend.service.d || true
-sudo touch /etc/systemd/system/sausage-store-backend.service.d/override.conf
-sudo -s
 PSQL_HOST=${PSQL_HOST}
 PSQL_DBNAME=${PSQL_DBNAME}
 PSQL_USER=${PSQL_USER}
@@ -18,6 +10,14 @@ MONGO_USER=${MONGO_USER}
 MONGO_PASSWORD=${MONGO_PASSWORD}
 MONGO_URL=${MONGO_URL}
 MONGO_DATABASE=${MONGO_DATABASE}
+
+#Перезаливаем дескриптор сервиса на ВМ для деплоя
+sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.jar
+sudo cp ./sausage-store-$VERSION.jar /home/jarservice/sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
+sudo mkdir /etc/systemd/system/sausage-store-backend.service.d || true
+sudo touch /etc/systemd/system/sausage-store-backend.service.d/override.conf
+sudo -s
 cat <<EOF >/etc/systemd/system/sausage-store-backend.service.d/override.conf
 Environment="PSQL_HOST=$PSQL_HOST"
 Environment="PSQL_PORT=6432"
