0a58052ef55217b4e35d59c3091c0360f0835e59
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-02-16 08:50:18 +0300

    Test-018

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -13,8 +13,8 @@ MONGO_DATABASE=${MONGO_DATABASE}
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf ./sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.jar
-sudo cp ./sausage-store-$VERSION.jar /home/jarservice/sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
-touch ~/sausage-unit-variables.conf.temp || true
+sudo cp ./sausage-store-$VERSION.jar /home/student/sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
+# touch ~/sausage-unit-variables.conf.temp || true
 cat <<EOF > ~/sausage-unit-variables.conf.temp
 Environment="PSQL_HOST='$PSQL_HOST'"'
 Environment="PSQL_PORT=6432"
