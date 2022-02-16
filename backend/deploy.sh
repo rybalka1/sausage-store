80d261d01acdc31d3e887c7c5c7aefc1906d8f7f
Дмитрий Рыбалка <rybalka.dmitrii@gmail.com>
2022-02-16 09:34:34 +0300

    Test-021

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -13,7 +13,7 @@ MONGO_DATABASE=${MONGO_DATABASE}
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp ./sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}//repository/sausage-store-rybalka-dmitrii-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-sudo cp ./sausage-store-${VERSION}.jar ./sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
+# sudo cp ./sausage-store-${VERSION}.jar ./sausage-store.jar || true #"jar||true" говорит, если команда обвалится — продолжай
 # touch ~/sausage-unit-variables.conf.temp || true
 cat <<EOF > ~/sausage-unit-variables.conf.temp
 [Service]
